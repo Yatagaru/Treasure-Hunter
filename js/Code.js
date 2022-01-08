31ef13e24effb5706c28b90ec4be3994583ed829
Yatagaru <69464040+Yatagaru@users.noreply.github.com>
2022-01-08 17:06:02 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,22 @@
+const accessCode1 = "VARIAVEL";
+const accessCode2 = "ADAPTABILIDADE";
+const accessCode3 = "EQUILIBRIO";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  // Adicione código para exibir as outras duas perguntas.
+  fill("white");
+  textSize(15);
+  fill("lightblue")
+  text("Dica: Elemento que define quanto mais o jogo avança, mais dificil fica",520,180)
+  
+  fill("white");
+  textSize(15);
+  fill("lightblue")
+  text("Dica: Sou o responsável por manter a ordem no jogo",300,340)
+  
+}
